3d917fde5c299ff409db28b034bddf937a59bff5
BloggerLife <macarnoldnenyasha@gmail.com>
2025-02-18 21:57:38 +0200

    initial

---
+++ src/app/page.jsx
@@ -0,0 +1,48 @@
+import Link from "next/link";
+import CallToAction from "./components/CallToAction";
+import RecentPosts from "./components/RecentPosts";
+
+export default async function Home() {
+  let posts = null;
+  try {
+    const result = await fetch(process.env.URL + "/api/post/get", {
+      method: "POST",
+      body: JSON.stringify({ limit: 9, order: "desc" }),
+      cache: "no-store",
+    });
+    const data = await result.json();
+    posts = data.posts;
+  } catch (error) {
+    console.log("Error getting post:", error);
+  }
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <div className="flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto ">
+        <h1 className="text-3xl font-bold lg:text-6xl">Welcome to WesayBlog</h1>
+        <p className="text-gray-500 text-sm sm:text-base">
+          Discover a variety of articles and insights on topics such as mental
+          health, personal developmnet, and travel experiences, all brought to
+          you by experts, enthusiast and learners!
+        </p>
+        <Link
+          href="/search"
+          className="text-xs sm:text-sm text-[#FF492E] font-bold hover:underline"
+        >
+          View all posts
+        </Link>
+      </div>
+      <div className="p-3 bg-amber-100 dark:bg-slate-700">
+        <CallToAction />
+      </div>
+      <div className="p-3 flex flex-col gap-8 py-7">
+        <RecentPosts limit={9} />
+        <Link
+          href={"/search?category=null"}
+          className="text-lg text-[#FF492E] hover:underline text-center"
+        >
+          View all posts
+        </Link>
+      </div>
+    </div>
+  );
+}
